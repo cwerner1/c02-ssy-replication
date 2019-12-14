a9f52a41f4abf522a19566885fe409ef3d980176
Christian Werner <cw@rizzistrasse.at>
2019-12-14 10:30:32 +0100

    Anpassung der Replication auf alle Server,
    wartet das auf allen geschrieben worden ist

--- routes/store.js
+++ routes/store.js
@@ -33,8 +33,7 @@ function putItem(req, res) {
     }
     // wir antworten schon, *bevor* wir Antwort von den Replikas haben.
     // Implikation?
-    res.json(item.value);
-    replicate(req);
+    replicate(req, res, item.value);
 }
 
 function delItem(req, res) {
@@ -44,20 +43,22 @@ function delItem(req, res) {
     } else {
         let item = items[0];
         collection.remove(item);
-        res.json(item.value);
+        replicate(req, res, item.value)
     }
-    replicate(req)
+
 }
 
 
-function replicate(req) {
+function replicate(req, response, item) {
     // falls die URL schon ?source angegeben hat, ist diese Instanz *Ziel* einer Replikation
     // --> nicht nochmal weiter replizieren
     if (req.query.source > 0) {
+        response.json(item);
         return;
     }
 
     // sonst alle Server durchgehen
+    let response_count = 0;
     for (let i = 1; i <= global.server_count; i++) {
         if (i == global.server_id) {
             // mit Ausnahme des eigenen
@@ -66,10 +67,19 @@ function replicate(req) {
 
         // und selben Request dorthin senden
         let port = 3000 + i - 1;
+
         let url = util.format('http://127.0.0.1:%d/store/%s?source=%d', port, req.params.id, global.server_id);
         Request({method: req.method, url: url, json: req.body, timeout: 500}, logResponse);
 
         function logResponse(err, resp, body) {
+            response_count++;
+            console.log("got" + response_count);
+            if (response_count === (global.server_count - 1)) {
+                // alle Antworten erhalten
+                console.log("got" + response_count);
+                response.json(item);
+
+            }
             // wir werten nichts aus, sondern in einem Fehlerfall geben wir nur eine Log-Meldung aus.
             // was bedeutet das für die Datensicherheit im Cluster?
             if (err) {
